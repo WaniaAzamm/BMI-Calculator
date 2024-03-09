f62a6ed1b6247c76ad3ce39246ee655f04220686
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-10 01:22:14 +0500

    Add files via upload

---
+++ BMI calculator/main.js
@@ -0,0 +1,4 @@
+var weightInKg = 70;
+var heightInMeter = 1.75;
+var bmi = weightInKg / (heightInMeter * heightInMeter);
+console.log("your bmi is ".concat(bmi, "."));
---
+++ BMI calculator/main.ts
@@ -0,0 +1,4 @@
+ let weightInKg = 70;
+ let heightInMeter = 1.75;
+ let bmi = weightInKg /(heightInMeter * heightInMeter);
+ console.log(`your bmi is ${bmi}.`);
